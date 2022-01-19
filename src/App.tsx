3e904cea58ad6bc8bec20a1f1697763fcd0fc054
Elzbieta Wrobel <elzbieta.wrobel@solarwinds.com>
2022-01-19 17:28:29 +0100

    menu sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Sidebar from './components/Sidebar';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 
-import logo from './logo.svg';
-import './App.css';
-
-// const Button = styled.button``
-
-function App() {
+const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          <p>Lorem ipsum dolor</p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />            
+        </Routes>
+      </Router> 
+    </>
+  )
 }
 
-export default App;
+export default App
